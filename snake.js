115d0becf9b7480104291bb469bd135b896d86d4
dschweizer <david.schweizer@inovex.de>
2020-11-25 17:30:54 +0100

    Snake can now get a tail.
    Fruits can be simulated by pressing space

--- snake.js
+++ snake.js
@@ -6,12 +6,12 @@ const LEFT = "left;"
 const UP = "up";
 const DOWN = "down"
 
-let game = setInterval(draw,100);
+let game = setInterval(draw,300);
 
 //player position
 //Point position = new Point(x,y)
-var x = 0;
-var y = 0;
+var x = [0];
+var y = [0];
 var direction;
 
 document.addEventListener("keydown",changeDirection)
@@ -20,6 +20,7 @@ function changeDirection(event) {
     var key = event.keyCode;
     
     switch(key) {
+        case 32: eat(); break;
         case 37: direction = LEFT; break;
         case 38: direction = UP; break;
         case 39: direction = RIGHT; break;
@@ -28,35 +29,53 @@ function changeDirection(event) {
 }
 
 function move() {
+
+
+    for(var i = x.length-1; i>0; i--){
+        x[i] = x[i-1];
+        y[i] = y[i-1];
+    }
+
+
     switch(direction){
-        case UP: y -=20; break;
-        case DOWN: y+=20; break;
-        case RIGHT: x+=20; break;
-        case LEFT: x-=20; break;
+        case UP: y[0]-=20; break;
+        case DOWN: y[0]+=20; break;
+        case RIGHT: x[0]+=20; break;
+        case LEFT: x[0]-=20; break;
     }
     checkCollisionWithBorder();
 }
 
 function checkCollisionWithBorder() {
-    if(x < 0)
-        x= 0;
-    if(x > 580)
-        x = 580;
-    if(y < 0)
-        y=0;
-    if(y > 580)
-        y=580;
+    if(x[0] < 0)
+        x[0] = 0;
+    if(x[0] > 580)
+        x[0] = 580;
+    if(y[0] < 0)
+        y[0]=0;
+    if(y[0] > 580)
+        y[0]=580;
 }
 
 function draw() {
     ctx.fillStyle="black";
     ctx.fillRect(0,0,cvs.width, cvs.height);
-    
-        
+    drawSnake();    
     move();
+    
+}
 
-    ctx.fillStyle="red";
-    ctx.fillRect(x,y, 20, 20);
+function eat() {
+    x.push(x[x.length-1]);
+    y.push(y[y.length-1])
+    
+}
+
+function drawSnake(){
+    for(var i = 0; i < x.length; i++) {
+        ctx.fillStyle="red";
+        ctx.fillRect(x[i],y[i], 20, 20);
+    }
 }
 
 
